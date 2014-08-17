1f75f1424f753220aa2c07ccf0b9f5f5418985a9
Mikimoto <mikimoto.c@gmail.com>
2014-08-17 21:05:03 +0800

    新增 scrapy project

---
+++ CabuKcgCrawler/settings.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+# Scrapy settings for CabuKcgCrawler project
+#
+# For simplicity, this file contains only the most important settings by
+# default. All the other settings are documented here:
+#
+#     http://doc.scrapy.org/en/latest/topics/settings.html
+#
+
+BOT_NAME = 'CabuKcgCrawler'
+
+SPIDER_MODULES = ['CabuKcgCrawler.spiders']
+NEWSPIDER_MODULE = 'CabuKcgCrawler.spiders'
+
+# Crawl responsibly by identifying yourself (and your website) on the user-agent
+#USER_AGENT = 'CabuKcgCrawler (+http://www.yourdomain.com)'
