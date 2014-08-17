1d44b0287af3fd90a555af54303db1fdb3e83a99
Mikimoto <mikimoto.c@gmail.com>
2014-08-18 00:26:29 +0800

    finish print data

--- CabuKcgCrawler/spiders/crawler61B3.py
+++ CabuKcgCrawler/spiders/crawler61B3.py
@@ -20,12 +20,34 @@ class CabuKcgCrawler61B3(Spider):
         @scrapes name
         """
         sel = Selector(response)
+
+        totalcols = 0
         headers = sel.xpath('//table[@id="dgPeopleStatis"]/tr[1]/th/text()')
-        print "-- Table headers"
+        print "-- Table headers --"
         for header in headers:
             print header.extract()
+            totalcols = totalcols + 1
 
+        dates = sel.xpath('//table[@id="dgPeopleStatis"]/tr/td[1]/font/span/text()')
         rols = sel.xpath('//table[@id="dgPeopleStatis"]/tr/td/text()')
+        print "Total Cols = ", totalcols
+        i = 0
+        dateIter = iter(dates)
+        print "-- Table content --"
+        for rol in rols:
+        	if rol.extract().replace('\r','').replace('\n','').replace('\t','') != "":
+        		#print "Col = ",i
+        		if i % (totalcols-1) == 0:
+        			date = next(dateIter)
+        			print date.extract()
+        			print rol.extract()
+        		else:
+        			print rol.extract()
+        		i = i + 1
+        	#else:
+        	#	print "Remove: ",rol
+
+
         items = []
 
 #        for rol in rols:
