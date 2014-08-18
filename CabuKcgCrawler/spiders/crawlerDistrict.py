1ecb91c3f251af047f7219b71ea53cb3335904bd
Mikimoto <mikimoto.c@gmail.com>
2014-08-18 17:30:06 +0800

    finish crawl districts

--- CabuKcgCrawler/spiders/crawlerDistrict.py
+++ CabuKcgCrawler/spiders/crawlerDistrict.py
@@ -1,3 +1,5 @@
+# -*- coding: UTF-8 -*-
+
 from scrapy.spider import Spider
 from scrapy.selector import Selector
 
@@ -6,34 +8,55 @@ from CabuKcgCrawler.items import CabukcgcrawlerItem
 import io,json,scrapy
 
 class CabuKcgCrawlerDistrict(Spider):
-    name = "District"
-    allowed_domains = ["gov.tw"]
-    start_urls = [
-        "http://cabu.kcg.gov.tw/cabu2/statis61B3.aspx"
-    ]
+		name = "District"
 
-    districts = []
+		def __init__(self):
+			self.allowed_domains = ["gov.tw"]
+			self.start_urls = ["http://cabu.kcg.gov.tw/cabu2/statis61B3.aspx"]
+			self.districts = []
+			self.districtsCount = 0
+			self.districtsTotal = 0
+			self.items = []
 
-    def parse(self, response):
-    	sel = Selector(response)
+		def parse(self, response):
+			sel = Selector(response)
+			self.districts = sel.xpath('//select[@id="ddlArea"]/option/@value').extract()
 
-    	districts = sel.xpath('//select[@id="ddlArea"]/option/@value').extract()
+			for district in self.districts:
+				self.districtsTotal = self.districtsTotal + 1
+				#print district
 
-    	for district in districts:
-    		print district
-
-    	return scrapy.FormRequest.from_response(
+			return scrapy.FormRequest.from_response(
     		response,
-    		formdata={'ddlArea': districts[2], 'LinkButton1': ''},
-    		callback=self.after_login
+    		formdata={'ddlArea': self.districts[self.districtsCount], 'LinkButton1': ''},
+    		callback=self.submit_district
       )
 
-    def after_login(self, response):
-    	sel = Selector(response)
+		def submit_district(self, response):
+			#print "Count = ", self.districtsCount, " Value = ", self.districts[self.districtsCount]
 
-    	villages = sel.xpath('//select[@id="ddlLi"]/option/@value').extract()
+			sel = Selector(response)
+			villages = sel.xpath('//select[@id="ddlLi"]/option/@value').extract()
+			filterVillages = []
+			for village in villages:
+				if village != u'合計':
+					filterVillages.append(village)
 
-    	for village in villages:
-    		print village
+			data = {self.districts[self.districtsCount]: filterVillages}
+			self.items.append(data)
 
-    	return None
+			self.districtsCount = self.districtsCount + 1
+			if self.districtsCount >= self.districtsTotal:
+				self.output()
+				return
+
+			return scrapy.FormRequest.from_response(
+    		response,
+    		formdata={'ddlArea': self.districts[self.districtsCount], 'LinkButton1': ''},
+    		callback=self.submit_district
+      )
+
+		def output(self):
+			print json.dumps(self.items, ensure_ascii=False)
+			with io.open('data/district.json', 'w', encoding='utf-8') as f:
+				f.write(unicode(json.dumps(self.items, ensure_ascii=False)))
