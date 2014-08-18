4a02a26c380b5826c221b02224bf993db1f82e1d
Mikimoto <mikimoto.c@gmail.com>
2014-08-18 15:07:05 +0800

    crawl village

--- CabuKcgCrawler/spiders/crawlerDistrict.py
+++ CabuKcgCrawler/spiders/crawlerDistrict.py
@@ -3,7 +3,7 @@ from scrapy.selector import Selector
 
 from CabuKcgCrawler.items import CabukcgcrawlerItem
 
-import io,json
+import io,json,scrapy
 
 class CabuKcgCrawlerDistrict(Spider):
     name = "District"
@@ -12,13 +12,28 @@ class CabuKcgCrawlerDistrict(Spider):
         "http://cabu.kcg.gov.tw/cabu2/statis61B3.aspx"
     ]
 
+    districts = []
+
     def parse(self, response):
     	sel = Selector(response)
-    	items = []
 
     	districts = sel.xpath('//select[@id="ddlArea"]/option/@value').extract()
 
     	for district in districts:
     		print district
+
+    	return scrapy.FormRequest.from_response(
+    		response,
+    		formdata={'ddlArea': districts[2], 'LinkButton1': ''},
+    		callback=self.after_login
+      )
+
+    def after_login(self, response):
+    	sel = Selector(response)
+
+    	villages = sel.xpath('//select[@id="ddlLi"]/option/@value').extract()
+
+    	for village in villages:
+    		print village
 
     	return None
