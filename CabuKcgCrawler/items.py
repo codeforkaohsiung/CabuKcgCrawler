c8db5dfa1d6559783eb72703c40f92c132318381
Mikimoto <mikimoto.c@gmail.com>
2014-08-21 16:26:50 +0800

    coding crawel
    不知道為何，鼓山區山下里 會噴 500

--- CabuKcgCrawler/items.py
+++ CabuKcgCrawler/items.py
@@ -1,48 +1,97 @@
 # -*- coding: utf-8 -*-
 
-# Define here the models for your scraped items
-#
-# See documentation in:
-# http://doc.scrapy.org/en/latest/topics/items.html
 
 from scrapy.item import Item, Field
 
 
+class HasNextWrapper(object):
+
+  def __init__(self, it):
+    self.it = iter(it)
+    self._hasnext = None
+
+  def __iter__(self): return self
+    
+  def next(self):
+    if self._hasnext:
+      result = self._thenext
+      print "result = ", result
+    else:
+      result = next(self.it)
+    
+    self._hasnext = None    
+    return result
+
+  def hasNext(self):
+    if self._hasnext is None:
+      try: 
+        self._thenext = next(self.it)
+        self._hasnext = True
+      except StopIteration: 
+        self._hasnext = False
+    
+    return self._hasnext
+
 class Singleton(type):
-    _instances = {}
-    def __call__(cls, *args, **kwargs):
-        if cls not in cls._instances:
-            cls._instances[cls] = super(Singleton, cls).__call__(*args, **kwargs)
-        return cls._instances[cls]
+  _instances = {}
+    
+  def __call__(cls, *args, **kwargs):
+    if cls not in cls._instances:
+      cls._instances[cls] = super(Singleton, cls).__call__(*args, **kwargs)
+    
+    return cls._instances[cls]
 
 class CabukcgcrawlerItem(Item):
-    # define the fields for your item here like:
-    # name = scrapy.Field()
-    name = Field()
-    description = Field()
-    url = Field()
+  name = Field()
+  description = Field()
+  url = Field()
 
 class CabuKcgVillageFactory:
-    __metaclass__ = Singleton
+  __metaclass__ = Singleton
 
-    def __init__(self):
-    	pass
+  def __init__(self):
+    self.items = []
+    self.processPoint = None
 
-    def loadFromJSON(self, villages):
-    	for district in villages:
-    		keys = district.iterkeys()
-    		for key in keys:
-    			for v in district[key]:
-    				item = CabuKcgVillageData()
-    				item['district'] = key
-    				item['village'] = v
-    				print item
+  def loadFromJSON(self, villages):
+    for district in villages:
+      keys = district.iterkeys()
+      
+      for key in keys:
+        for v in district[key]:
+          item = CabuKcgVillageData()
+          item['district'] = key
+          item['village'] = v
+          item['headers'] = None
+          item['data'] = None
+          self.items.append(item)
 
-    	return None
+      self.processPoint = HasNextWrapper(self.items)    
+
+  def hasNext(self):
+    return self.processPoint.hasNext()
+
+  def next(self):
+    return self.processPoint.next()
+
+  def printSelf(self):
+    for item in self.items:
+      print item
+
+  def fillOfData(self, district, village, headers, data):
+    for it in self.items:
+      if it['district'] == district and it['village'] == village:
+        it['headers'] = headers
+        it['data'] = data
+        break
+
 
 class CabuKcgVillageData(Item):
-	district = Field()
-	village = Field()
+  district = Field()
+  village = Field()
+  headers = Field()
+  data = Field()
 
-	def __str__(self):
-		return '[{0} - {1}]'.format(unicode(self['district']).encode('utf-8'), unicode(self['village']).encode('utf-8'))
+
+  def __str__(self):
+    return '[{0} - {1}] Data => {2}'.format(unicode(self['district']).encode('utf-8'), unicode(self['village']).encode('utf-8'), self['data'])
--- CabuKcgCrawler/spiders/crawler61B3.py
+++ CabuKcgCrawler/spiders/crawler61B3.py
@@ -6,77 +6,64 @@ from CabuKcgCrawler.items import CabuKcgVillageData, CabuKcgVillageFactory
 import io,json,scrapy
 
 class CabuKcgCrawler61B3(Spider):
-    name = "61B3"
+  name = "61B3"
+  allowed_domains = ["gov.tw"]
+  start_urls = [
+    "http://cabu.kcg.gov.tw/cabu2/statis61B3.aspx"
+  ]
+  rate = 1
 
-    def __init__(self):
-        self.allowed_domains = ["gov.tw"]
-        self.start_urls = [
-            "http://cabu.kcg.gov.tw/cabu2/statis61B3.aspx"
-        ]
-        self.districtsData = None
-        self.districtsKey = 0
+  def __init__(self):
+    self.districtData = None
+    self.download_delay = 2
 
-    def parse(self, response):
+  def next(self, response):    
+    if CabuKcgVillageFactory().hasNext() != True:
+      CabuKcgVillageFactory().printSelf()
+      return None
 
-        with io.open('data/district.json', 'r', encoding='utf-8') as f:
-            self.districtsData = json.load(f)
-            CabuKcgVillageFactory().loadFromJSON(self.districtsData)
-            #print self.districtsData
-
-        sel = Selector(response)
-        items = []
-
-        totalcols = 0
-        item = []
-        headers = sel.xpath('//table[@id="dgPeopleStatis"]/tr[1]/th/text()')
-        #print "-- Table headers --"
-        for header in headers:
-        	item.append(header.extract())
-        	#print header.extract()
-        	totalcols = totalcols + 1
-
-        dates = sel.xpath('//table[@id="dgPeopleStatis"]/tr/td[1]/font/span/text()')
-        rols = sel.xpath('//table[@id="dgPeopleStatis"]/tr/td/text()')
-        #print "Total Cols = ", totalcols
-        i = 0
-        dateIter = iter(dates)
-        #print "-- Table content --"
-        for rol in rols:
-        	if rol.extract().replace('\r','').replace('\n','').replace('\t','') != "":
-        		#print "Col = ",i
-        		if i % (totalcols-1) == 0:
-        			items.append(item)
-        			item = []
-        			date = next(dateIter)
-        			item.append(date.extract())
-        			#print date.extract()
-        			item.append(rol.extract())
-        			#print rol.extract()
-        		else:
-        			item.append(rol.extract())
-        			#print rol.extract()
-        		i = i + 1
-        	#else:
-        	#	print "Remove: ",rol
-
-        #print json.dumps(items, ensure_ascii=False)
-        #with io.open('data/61B3-json.txt', 'w', encoding='utf-8') as f:
-        #	f.write(unicode(json.dumps(items, ensure_ascii=False)))
-
-#        for rol in rols:
-#            item = Website()
-#            item['name'] = site.xpath('a/text()').extract()
-#            item['url'] = site.xpath('a/@href').extract()
-#            item['description'] = site.xpath('text()').re('-\s([^\n]*?)\\n')
-#            items.append(item)				
-				#print json.dumps(items, ensure_ascii=False)
-				#with io.open('data.txt', 'w', encoding='utf-8') as f:
-				#	f.write(unicode(json.dumps(items, ensure_ascii=False)))
-        return scrapy.FormRequest.from_response(
+    self.districtData = CabuKcgVillageFactory().next()
+    print "ddlArea = [",self.districtData['district'],"] ddlLi = [", self.districtData['village'], "]. "
+    return scrapy.FormRequest.from_response(
             response,
-            formdata={'ddlArea': self.districtsData[0], 'LinkButton1': ''},
+            formdata={'ddlArea': self.districtData['district'], 'ddlLi': self.districtData['village']},
             callback=self.submit_district
-        )
+    )
 
-    def submit_district(self, response):
-        pass
+
+  def parse(self, response):
+    with io.open('data/district.json', 'r', encoding='utf-8') as f:
+      CabuKcgVillageFactory().loadFromJSON(json.load(f))
+    return self.next(response)
+
+  def submit_district(self, response):
+    sel = Selector(response)
+    itemHeader = []
+    items = []
+
+    headers = sel.xpath('//table[@id="dgPeopleStatis"]/tr/th/text()')
+    for header in headers:
+      itemHeader.append(header.extract())
+
+    dates = sel.xpath('//table[@id="dgPeopleStatis"]/tr/td[1]/font/span/text()')
+    rols = sel.xpath('//table[@id="dgPeopleStatis"]/tr/td/text()')
+
+    i = 0
+    item = None
+    dateIter = iter(dates)
+    for rol in rols:
+      if rol.extract().replace('\r','').replace('\n','').replace('\t','') != "":
+        if i % (len(itemHeader) - 1) == 0:
+          if item != None:
+            items.append(item)
+          item = []
+          date = next(dateIter)
+          item.append(date.extract())
+          item.append(rol.extract())
+        else:
+          item.append(rol.extract())
+
+        i = i + 1
+    
+    CabuKcgVillageFactory().fillOfData(self.districtData['district'], self.districtData['village'], itemHeader, items)
+    return self.next(response)
