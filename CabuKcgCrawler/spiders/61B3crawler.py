29c5c3ac23c14bbc5df1e00e0a9b8c4e1a18aaec
Mikimoto <mikimoto@drsays.org>
2014-12-18 19:20:13 +0800

    debug 61B3

--- CabuKcgCrawler/spiders/61B3crawler.py
+++ CabuKcgCrawler/spiders/61B3crawler.py
@@ -4,164 +4,142 @@ from scrapy.spider import Spider
 from scrapy.selector import Selector
 from scrapy.shell import inspect_response
 
-from CabuKcgCrawler.items import CabuKcgVillageData, CabuKcgDistrictItem
-from CabuKcgCrawler.utils import CabuKcgVillageFactory
+from CabuKcgCrawler.utils import CabuKcgDataItem, CabuKcg61B3TableRow
+from CabuKcgCrawler.utils import CabuKcgVillageFactory, CabuKcgStringProcess
 
-from pprint import pprint
+import pprint
 
-import io,json,scrapy
+import io
+import json
+import scrapy
 import HTMLParser
 
 
 class CabuKcgCrawler61B3(Spider):
-  name = "61B3"
-  allowed_domains = ["gov.tw"]
-  start_urls = [
-    "http://cabu.kcg.gov.tw/cabu2/statis61B3.aspx"
-  ]
-  pipelines = ["C61B3JSONWritePipeline"]
+    name = "61B3"
+    allowed_domains = ["gov.tw"]
+    start_urls = ["http://cabu.kcg.gov.tw/cabu2/statis61B3.aspx"]
+    pipelines = ["C61B3JSONWritePipeline"]
 
-  def __init__(self):
-    self.districtItems = []
-    self.districtData = None
-    self.currentDistrict = None
-    self.download_delay = 0.5
+    def __init__(self):
+        self.districtItems = []
+        self.districtData = None
+        self.currentDistrict = None
+        self.download_delay = 0.5
 
-  def next(self, response):
-    #pprint(self.districtItems)
-    #CabuKcgVillageFactory().printSelf()
-    #inspect_response(response)
-
-    if CabuKcgVillageFactory().hasNext() != True:
-    #  CabuKcgVillageFactory().printSelf()
-      return None
-
-    if self.districtData != None and self.districtData.district == u'小港區':
-      if self.districtData.village == u'宏亮里' or self.districtData.village == u'店鎮里' or self.districtData.village == u'松金里' or self.districtData.village == u'桂林里' or self.districtData.village == u'廈莊里' or self.districtData.village == u'鳳宮里' or self.districtData.village == u'鳳源里' or self.districtData.village == u'鳳興里' or self.districtData.village == u'濟南里　':
-    #    inspect_response(response)
-    #    self.addSuffixes()
-        self.districtData = CabuKcgVillageFactory().next()
-        #self.districtData.village = self.districtData.village + u'　'
-
-    #if self.districtData['district'] == u'鳳山二':
-    #  if self.districtData['village'] == u'二甲里' or self.districtData['village'] == u'中民里' or self.districtData['village'] == u'中榮里' or self.districtData['village'] == u'天興里' or self.districtData['village'] == u'武漢里' or self.districtData['village'] == u'保安里' or self.districtData['village'] == u'南和里' or self.districtData['village'] == u'國光里' or self.districtData['village'] == u'國富里' or self.districtData['village'] == u'富甲里' or self.districtData['village'] == u'善美里' or self.districtData['village'] == u'新武里' or self.districtData['village'] == u'新強里' or self.districtData['village'] == u'新樂里' or self.districtData['village'] == u'福祥里' or self.districtData['village'] == u'福興里' or self.districtData['village'] == u'鎮南里':
-    #    self.addSuffixes()
-        #self.districtData = CabuKcgVillageFactory().next()
-
-    #if self.districtData['district'] == u'林園區':
-    #  if self.districtData['village'] == u'西溪里' or self.districtData['village'] == u'東林里' or self.districtData['village'] == u'林家里' or self.districtData['village'] == u'頂厝里' or self.districtData['village'] == u'港嘴里' or self.districtData['village'] == u'鳳芸里' or self.districtData['village'] == u'潭頭里':
-    #    self.addSuffixes()
-        #self.districtData = CabuKcgVillageFactory().next()
-
-    #if self.districtData['district'] == u'大寮區':
-    #  if self.districtData['village'] == u'上寮里' or self.districtData['village'] == u'山頂里':
-    #    self.addSuffixes()
-        #self.districtData = CabuKcgVillageFactory().next()
-
-    #if self.districtData['district'] == u'大樹區':
-    #  if self.districtData['village'] == u'和山里' or self.districtData['village'] == u'統嶺里' or self.districtData['village'] == u'興山里' or self.districtData['village'] == u'龍目里':
-    #    self.addSuffixes()
-        #self.districtData = CabuKcgVillageFactory().next()
-
-    #if self.districtData['district'] == u'大社區':
-    #  if self.districtData['village'] == u'保社里' or self.districtData['village'] == u'嘉誠里' or self.districtData['village'] == u'觀音里':
-    #    self.addSuffixes()
-        #self.districtData = CabuKcgVillageFactory().next()
-
-    #if self.districtData['district'] == u'路竹區':
-    #  if self.districtData['village'] == u'竹西里' or self.districtData['village'] == u'竹南里' or self.districtData['village'] == u'竹滬里' or self.districtData['village'] == u'社西里' or self.districtData['village'] == u'社南里' or self.districtData['village'] == u'頂寮里' or self.districtData['village'] == u'鴨寮里':
-    #    self.addSuffixes()
-        #self.districtData['village'] = self.districtData['village'] + u'　'
+    def next(self, response):
+        #pprint(self.districtItems)
+        #CabuKcgVillageFactory().printSelf()
         #inspect_response(response)
-        #self.districtData = CabuKcgVillageFactory().next()
 
-    #if self.districtData['district'] == u'六龜區':
-    #  if self.districtData['village'] == u'荖濃里':
-    #    self.addSuffixes()
-        #self.districtData['village'] = self.districtData['village'] + u'　'
+        if CabuKcgVillageFactory().hasNext() is not True:
+            return None
 
+        self.districtData = CabuKcgVillageFactory().next()
 
+        if self.currentDistrict is None or \
+                self.currentDistrict is not self.districtData.district:
+            self.currentDistrict = self.districtData.district
 
+        return scrapy.FormRequest.from_response(
+            response,
+            formdata={'ddlArea': self.currentDistrict, 'LinkButton1': ''},
+            callback=self.changeDistrict
+        )
 
-    self.districtData = CabuKcgVillageFactory().next()
+        #html_parser = HTMLParser.HTMLParser()
+        print "ddlArea = [", self.districtData.district, \
+            "] ddlLi = [", self.districtData.village, "]. "
 
-    if self.currentDistrict == None or self.currentDistrict != self.districtData.district:
-      self.currentDistrict = self.districtData.district
-      return scrapy.FormRequest.from_response(
-        response,
-        formdata={'ddlArea': self.currentDistrict, 'LinkButton1': ''},
-        callback=self.changeDistrict
-      )
+        return scrapy.FormRequest.from_response(
+            response,
+            formname="Form1",
+            dont_click=True,
+            formdata={'ddlArea': self.districtData.district,
+                        'ddlLi': self.districtData.village},
+            clickdata={'name': "btnSearch"},
+            meta={'http-equiv': "Content-Type",
+                     'content': "text/html; charset: UTF-8"},
+            callback=self.submit
+        )
 
-    #print "ddlArea = [",self.districtData['district'],"] ddlLi = [", self.districtData['village'], "]. "
+    def changeDistrict(self, response):
+        #print "ddlArea = [",self.districtData['district'],"] ddlLi = [", self.districtData['village'], "]. "
 
-    html_parser = HTMLParser.HTMLParser()
-    print "ddlArea = [",self.districtData.district,"] ddlLi = [",self.districtData.village, "]. "
-    #inspect_response(response)
+        html_parser = HTMLParser.HTMLParser()
+        print "ddlArea = [",self.districtData.district,"] ddlLi = [",self.districtData.village,"]. "
+        #inspect_response(response)
 
-    return scrapy.FormRequest.from_response(
+        return scrapy.FormRequest.from_response(
             response,
             formname="Form1",
             dont_click=True,
             formdata={'ddlArea': self.districtData.district, 'ddlLi': self.districtData.village},
             clickdata={'name': "btnSearch"},
             meta={'http-equiv': "Content-Type", 'content': "text/html; charset: UTF-8"},
-            callback=self.submit
-    )
+            callback=self.submit)
 
-  def changeDistrict(self, response):
-    #print "ddlArea = [",self.districtData['district'],"] ddlLi = [", self.districtData['village'], "]. "
+    def parse(self, response):
+        with io.open('data/raw/district.json', 'r', encoding='utf-8') as f:
+            CabuKcgVillageFactory().loadFromJSON(json.load(f))
+            f.close()
+        return self.next(response)
 
-    html_parser = HTMLParser.HTMLParser()
-    print "ddlArea = [",self.districtData.district,"] ddlLi = [",self.districtData.village, "]. "
-    #inspect_response(response)
+    def submit(self, response):
+        sel = Selector(response)
+        itemHeader = []
+        items = []
 
-    return scrapy.FormRequest.from_response(
-            response,
-            formname="Form1",
-            dont_click=True,
-            formdata={'ddlArea': self.districtData.district, 'ddlLi': self.districtData.village},
-            clickdata={'name': "btnSearch"},
-            meta={'http-equiv': "Content-Type", 'content': "text/html; charset: UTF-8"},
-            callback=self.submit
-    )    
+        headers = sel.xpath('//table[@id="dgPeopleStatis"]/tr/th/text()')
+        for header in headers:
+            itemHeader.append(header.extract())
 
-  def parse(self, response):
-    with io.open('data/raw/district.json', 'r', encoding='utf-8') as f:
-      CabuKcgVillageFactory().loadFromJSON(json.load(f))
-      f.close()
-    return self.next(response)
+        dates = sel.xpath(
+            '//table[@id="dgPeopleStatis"]/tr/td[1]/font/span/text()')
+        rols = sel.xpath('//table[@id="dgPeopleStatis"]/tr/td/text()')
 
-  def submit(self, response):
-    sel = Selector(response)
-    itemHeader = []
-    items = []
+        item = CabuKcg61B3TableRow()
+        #dateIter = iter(dates)
 
-    headers = sel.xpath('//table[@id="dgPeopleStatis"]/tr/th/text()')
-    for header in headers:
-      itemHeader.append(header.extract())
+        for x in xrange(0, len(dates)):
+            item.date = CabuKcgStringProcess().cleanTab(dates[x].extract())
 
-    dates = sel.xpath('//table[@id="dgPeopleStatis"]/tr/td[1]/font/span/text()')
-    rols = sel.xpath('//table[@id="dgPeopleStatis"]/tr/td/text()')
+            item.numberOfLin = CabuKcgStringProcess() \
+                .cleanTab(rols[3+(5*x)].extract())
 
-    i = 0
-    item = None
-    dateIter = iter(dates)
-    for rol in rols:
-      if rol.extract().replace('\r','').replace('\n','').replace('\t','') != "":
-        if i % (len(itemHeader) - 1) == 0:
-          if item != None:
+            item.numberOfFu = CabuKcgStringProcess() \
+                .cleanTab(rols[4+(5*x)].extract())
+
+            item.maleNumber = CabuKcgStringProcess() \
+                .cleanTab(rols[6+(5*x)].extract())
+
+            item.femaleNumber = CabuKcgStringProcess() \
+                .cleanTab(rols[7+(5*x)].extract())
+
             items.append(item)
-          item = []
-          date = next(dateIter)
-          item.append(date.extract())
-          item.append(rol.extract())
-        else:
-          item.append(rol.extract())
+            inspect_response(response)
 
-        i = i + 1
-    
-    CabuKcgVillageFactory().fillOfData(self.districtData.district, self.districtData.village, itemHeader, items)
-    #inspect_response(response)
+        for rol in rols:
+            cleanRol = CabuKcgStringProcess().cleanTab(rol.extract())
+            if len(cleanRol) is not 0:
+                if i % (len(itemHeader) - 1) == 0:
+                    if item is not None:
+                        items.append(item)
 
-    return self.next(response)
+                    item = CabuKcg61B3TableRow()
+                    date = next(dateIter)
+
+                    item.date = date.extract()
+                    item.append(cleanRol)
+
+                else:
+                    item.append(cleanRol)
+
+            i = i + 1
+        CabuKcgVillageFactory().fillOfData(
+            self.districtData.district, self.districtData.village,
+            itemHeader, items)
+        CabuKcgVillageFactory().printSelf()
+        inspect_response(response)
+
+        return self.next(response)
--- CabuKcgCrawler/utils.py
+++ CabuKcgCrawler/utils.py
@@ -2,82 +2,110 @@
 
 
 from patterns import Singleton
-from scrapy import log
 
 import pprint
 
 
-class CabuKcgDataItem():
-  district = None
-  village = None
-  headers = None
-  data = None
+class PrintSelfClass(object):
+    def printself(self):
+        pp = pprint.PrettyPrinter()
+        print "OK, Let's print all object ==>"
+        for item in self.items:
+            print "[", item.district, " (", item.village, ") ]"
+            pp.pprint(item.headers)
+            pp.pprint(item.data)
+
+
+class CabuKcgDataItem(object):
+    district = None
+    village = None
+    headers = None
+    data = None
+
+
+class CabuKcg61B3TableRow(object):
+    date = None
+    numberOfLin = 0
+    numberOfFu = 0
+    maleNumber = 0
+    femaleNumber = 0
 
 
 class HasNextWrapper(object):
 
-  def __init__(self, it):
-    self.it = iter(it)
-    self._hasnext = None
+    def __init__(self, it):
+        self.it = iter(it)
+        self._hasnext = None
 
-  def __iter__(self): return self
-    
-  def next(self):
-    if self._hasnext:
-      result = self._thenext
-    else:
-      result = next(self.it)
-    
-    self._hasnext = None    
-    return result
+    def __iter__(self):
+        return self
 
-  def hasNext(self):
-    if self._hasnext is None:
-      try: 
-        self._thenext = next(self.it)
-        self._hasnext = True
-      except StopIteration: 
-        self._hasnext = False
-    
-    return self._hasnext
+    def next(self):
+        if self._hasnext:
+            result = self._thenext
+        else:
+            result = next(self.it)
+
+        self._hasnext = None
+        return result
+
+    def hasNext(self):
+        if self._hasnext is None:
+            try:
+                self._thenext = next(self.it)
+                self._hasnext = True
+            except StopIteration:
+                self._hasnext = False
+
+        return self._hasnext
+
+
+class CabuKcgStringProcess:
+    __metaclass__ = Singleton
+
+    def __init__(self):
+        pass
+
+    def cleanTab(self, string):
+        return string.strip()
+
 
 class CabuKcgVillageFactory:
-  __metaclass__ = Singleton
+    __metaclass__ = Singleton
 
-  def __init__(self):
-    self.items = []
-    self.processPoint = None
+    def __init__(self):
+        self.items = []
+        self.processPoint = None
 
-  def loadFromJSON(self, jsons):
-    for js in jsons:
-      district = js['district']
-      villages = js['villages']
+    def loadFromJSON(self, jsons):
+        for js in jsons:
+            district = js['district']
+            villages = js['villages']
+            if district is not None and villages is not None:
+                for v in villages:
+                    item = CabuKcgDataItem()
+                    item.district = district
+                    item.village = v
+                    self.items.append(item)
+        self.processPoint = HasNextWrapper(self.items)
 
-      for v in villages:
-        item = CabuKcgDataItem()
-        item.district = district
-        item.village = v
-        self.items.append(item)
+    def hasNext(self):
+        return self.processPoint.hasNext()
 
-    self.processPoint = HasNextWrapper(self.items)    
+    def next(self):
+        return self.processPoint.next()
 
-  def hasNext(self):
-    return self.processPoint.hasNext()
+    def printSelf(self):
+        pp = pprint.PrettyPrinter()
+        print "OK, Let's print all object ==>"
+        for item in self.items:
+            print "[", item.district, " (", item.village, ") ]"
+            pp.pprint(item.headers)
+            pp.pprint(item.data)
 
-  def next(self):
-    return self.processPoint.next()
-
-  def printSelf(self):
-    pp = pprint.PrettyPrinter()
-    print "OK, Let's print all object ==>"
-    for item in self.items:
-      print "[",item.district," (",item.village,") ]"
-      pp.pprint(item.headers)
-      pp.pprint(item.data)
-
-  def fillOfData(self, district, village, headers, data):
-    for it in self.items:
-      if it.district == district and it.village == village:
-        it.headers = headers
-        it.data = data
-        break
+    def fillOfData(self, district, village, headers, data):
+        for it in self.items:
+            if it.district == district and it.village == village:
+                it.headers = headers
+                it.data = data
+                break
